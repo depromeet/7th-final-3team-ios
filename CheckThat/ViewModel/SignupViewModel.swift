6413125cebe313d7504f4b84170d3a132572bc63
Changnam Hong <hcn1519@gmail.com>
2019-12-26 18:00:42 +0900

    SignupViewModel 추가 및 테스트 환경 구축

---
+++ CheckThat/ViewModel/SignupViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  SignupViewModel.swift
+//  CheckThat
+//
+//  Created by 홍창남 on 2019/12/26.
+//  Copyright © 2019 hcn1519. All rights reserved.
+//
+
+import Foundation
+
+class SignupViewModel {
+
+    var emailStr: String = ""
+    var password: String = ""
+
+}
