768dd8c54b42d739506340c8a8d47cb5e9afbefe
Changnam Hong <hcn1519@gmail.com>
2019-12-26 18:00:42 +0900

    SignupViewModel 추가 및 테스트 환경 구축

---
+++ CheckThat/Extensions/String+Validation.swift
@@ -0,0 +1,9 @@
+//
+//  String+Validation.swift
+//  CheckThat
+//
+//  Created by 홍창남 on 2019/12/26.
+//  Copyright © 2019 hcn1519. All rights reserved.
+//
+
+import Foundation
---
+++ CheckThatTests/SignupTests.swift
@@ -0,0 +1,19 @@
+//
+//  SignupTests.swift
+//  CheckThatTests
+//
+//  Created by 홍창남 on 2019/12/26.
+//  Copyright © 2019 hcn1519. All rights reserved.
+//
+
+import XCTest
+@testable import CheckThat
+
+class SignupTests: XCTestCase {
+
+    var viewModel: SignupViewModel!
+
+    override func setUp() {
+        viewModel = SignupViewModel()
+    }
+}
