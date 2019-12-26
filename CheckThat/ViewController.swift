65c68f19ce33599fb6b4b3a60be8ab079810df87
Changnam Hong <hcn1519@gmail.com>
2019-12-26 16:44:50 +0900

    First Commit

---
+++ CheckThat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckThat
+//
+//  Created by 홍창남 on 2019/12/26.
+//  Copyright © 2019 hcn1519. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
