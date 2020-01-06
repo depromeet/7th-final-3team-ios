6c47c97da92da3b7c6ecf225b1c417a0de2dd671
Changnam Hong <hcn1519@gmail.com>
2020-01-06 23:17:25 +0900

    [#2] SignUp Validation 룰 재수정

--- WatNiTests/SignupTests.swift
+++ WatNiTests/SignupTests.swift
@@ -16,4 +16,48 @@ class SignupTests: XCTestCase {
     override func setUp() {
         viewModel = SignupViewModel()
     }
+
+    // MARK: Email Validation
+
+    func testIdentifierValid1() {
+        viewModel.email = "abcded"
+        XCTAssertFalse(viewModel.isValidEmail, "Wrong Email")
+    }
+
+    func testIdentifierValid2() {
+        viewModel.email = "a1w124@navercom"
+        XCTAssertFalse(viewModel.isValidEmail, "Wrong Email")
+    }
+
+    func testIdentifierValid3() {
+        viewModel.email = "abcded@gmail.com"
+        XCTAssert(viewModel.isValidEmail, "Email should be valid")
+    }
+
+    func testIdentifierValid4() {
+        viewModel.email = "ab-c123d.ed@naver.com"
+        XCTAssert(viewModel.isValidEmail, "Email should be valid")
+    }
+
+    // MARK: Password Validation
+
+    func testPasswordValid1() {
+        viewModel.password = ""
+        XCTAssertFalse(viewModel.isValidPassword, "Wrong Password")
+    }
+
+    func testPasswordValid2() {
+        viewModel.password = "1234567890123"
+        XCTAssertFalse(viewModel.isValidPassword, "Wrong Password")
+    }
+
+    func testPasswordValid3() {
+        viewModel.password = "123456"
+        XCTAssert(viewModel.isValidPassword, "Password should be valid")
+    }
+
+    func testPasswordValid4() {
+        viewModel.password = "123456sd"
+        XCTAssert(viewModel.isValidPassword, "Password should be valid")
+    }
 }
