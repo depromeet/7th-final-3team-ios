0d50d9ce507cdc05f5b6dce5dbd302aabcd4d362
Changnam Hong <hcn1519@gmail.com>
2020-01-06 23:17:25 +0900

    [#2] SignUp Validation 룰 재수정

--- WatNi/ViewModel/SignupViewModel.swift
+++ WatNi/ViewModel/SignupViewModel.swift
@@ -10,7 +10,21 @@ import Foundation
 
 class SignupViewModel {
 
-    var emailStr: String = ""
+    var email: String = ""
     var password: String = ""
 
+    /// Validate Email
+    /// - Parameter candidate: Email String
+    var isValidEmail: Bool {
+        let expression = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let result = email.range(of: expression, options: .regularExpression)
+
+        return result != nil
+    }
+
+    /// Validate Password
+    /// - Parameter candidate: Password String
+    var isValidPassword: Bool {
+        return password.count >= 6 && password.count <= 12
+    }
 }
