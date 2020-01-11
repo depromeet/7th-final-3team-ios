d1b4bb01a34c8f4212b44eac3a1e93d4cb07a242
Changnam Hong <hcn1519@gmail.com>
2020-01-11 20:07:06 +0900

    [#15] Main StoryBoard 제거
    OnBoardingViewController를 root로 설정

--- WatNi/SceneDelegate.swift
+++ WatNi/SceneDelegate.swift
@@ -13,10 +13,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard (scene as? UIWindowScene) != nil else { return }
+
+        if let scene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: scene)
+
+            let onboardingViewController = OnBoardingViewController(nibName: OnBoardingViewController.className, bundle: nil)
+            let navigationController = UINavigationController(rootViewController: onboardingViewController)
+            window.rootViewController = navigationController
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
