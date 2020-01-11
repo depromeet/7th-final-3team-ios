7e71e47bc00bd35bdf5df13d568d6e80c951ed25
Changnam Hong <hcn1519@gmail.com>
2020-01-11 20:07:06 +0900

    [#15] Main StoryBoard 제거
    OnBoardingViewController를 root로 설정

---
+++ WatNi/ViewController/OnBoardingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OnBoardingViewController.swift
+//  WatNi
+//
+//  Created by 홍창남 on 2020/01/11.
+//  Copyright © 2020 hcn1519. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OnBoardingViewController: UIViewController {
+
+}
