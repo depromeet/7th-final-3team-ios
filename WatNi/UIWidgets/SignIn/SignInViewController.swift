271ce93e55ec030fe424483a29d3d7092dd7202e
Changnam Hong <hcn1519@gmail.com>
2020-01-19 00:21:07 +0900

    [#20] SignIn 화면 TextField 구성

--- WatNi/UIWidgets/SignIn/SignInViewController.swift
+++ WatNi/UIWidgets/SignIn/SignInViewController.swift
@@ -8,12 +8,17 @@
 
 import Foundation
 import UIKit
+import Combine
 
 class SignInViewController: UIViewController, ViewModelInjectable {
 
     typealias ViewModel = SignInViewModel
 
+    @IBOutlet private weak var emailTextFieldView: UnderlineTextFieldView!
+    @IBOutlet private weak var passwordTextFieldView: UnderlineTextFieldView!
+
     let viewModel: SignInViewModel
+    private var cancelables = Set<AnyCancellable>()
 
     required init(viewModel: SignInViewModel, nibName: String) {
         self.viewModel = viewModel
@@ -24,10 +29,57 @@ class SignInViewController: UIViewController, ViewModelInjectable {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        subscribeTextFieldEditingChanged()
+        emailTextFieldView.textField.delegate = self
+        passwordTextFieldView.textField.delegate = self
+    }
+
+    private func subscribeTextFieldEditingChanged() {
+        let emailViewModel = UnderlineTextFieldViewModel(descLabelStr: "이메일",
+                                                         inputContentType: .emailAddress,
+                                                         returnKeyType: .continue,
+                                                         keyboardType: .asciiCapable)
+
+        let passwordViewModel = UnderlineTextFieldViewModel(descLabelStr: "비밀번호",
+                                                            inputContentType: .password,
+                                                            returnKeyType: .done,
+                                                            keyboardType: .default)
+
+        emailTextFieldView.viewModel = emailViewModel
+        passwordTextFieldView.viewModel = passwordViewModel
+
+        let emailPublisher = emailTextFieldView.textField.publisher(for: .editingChanged)
+        let passwordPublisher = passwordTextFieldView.textField.publisher(for: .editingChanged)
+
+        emailPublisher.compactMap {
+            ($0 as? UITextField)?.text
+        }.assign(to: \UnderlineTextFieldViewModel.inputStr, on: emailViewModel)
+            .store(in: &cancelables)
+
+        passwordPublisher.compactMap {
+            ($0 as? UITextField)?.text
+        }.assign(to: \UnderlineTextFieldViewModel.inputStr, on: passwordViewModel)
+            .store(in: &cancelables)
+    }
+
     @IBAction func signInBtnTapped(_ sender: UIButton) {
 
         AuthProvider.issueToken(userName: "test@naver.com", password: "test") { result in
             print(result)
         }
+    }
+}
+
+extension SignInViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTextFieldView.textField {
+            passwordTextFieldView.textField.becomeFirstResponder()
+        } else if textField == passwordTextFieldView.textField {
+            passwordTextFieldView.textField.resignFirstResponder()
+        }
+        return true
     }
 }
