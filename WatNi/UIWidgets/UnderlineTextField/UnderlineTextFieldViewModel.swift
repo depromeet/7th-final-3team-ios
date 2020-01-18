d4ed8ab372982d383d0af2141dc956afa021b3e6
Changnam Hong <hcn1519@gmail.com>
2020-01-18 22:36:43 +0900

    [#22] 공통 TextField 구현
    - 기본 UI 랜더링 설정
    - input 값 validation closure 설정 로직 구현

---
+++ WatNi/UIWidgets/UnderlineTextField/UnderlineTextFieldViewModel.swift
@@ -0,0 +1,87 @@
+//
+//  UnderlineTextFieldViewModel.swift
+//  WatNi
+//
+//  Created by 홍창남 on 2020/01/16.
+//  Copyright © 2020 hcn1519. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol UnderlineTextFieldViewModelProtocol {
+    /// TextField 입력 문구
+    var inputStr: String { get set }
+    /// TextField Valid 여부
+    var inputIsValid: Bool { get }
+    /// TextField Valid 조건 설정
+    var validCondition: (() -> Bool)? { get set }
+    /// 상단 설명 문구
+    var descLabelStr: String { get }
+    /// TextField의 ContentType
+    var inputContentType: UITextContentType { get }
+    /// TextField의 ReturnKeyType
+    var returnKeyType: UIReturnKeyType { get }
+    /// TextField의 KeyboardType
+    var keyboardType: UIKeyboardType { get }
+    /// 하단 가이드 문구
+    var assistantLabelStr: String { get }
+    /// Input 값에 SecureText 사용 여부
+    var isSecureTextEntry: Bool { get }
+    /// FontSize: TextField
+    var textFieldFontSize: CGFloat { get }
+    /// LineHeight: TextField
+    var textFieldLineHeight: CGFloat { get }
+    /// FontSize: descLabel
+    var descLabelFontSize: CGFloat { get }
+    /// FontSize: assistantLabel
+    var assistantLabelFontSize: CGFloat { get }
+}
+
+class UnderlineTextFieldViewModel: UnderlineTextFieldViewModelProtocol {
+    let inputContentType: UITextContentType
+    let returnKeyType: UIReturnKeyType
+    let keyboardType: UIKeyboardType
+
+    var inputStr: String = ""
+
+    var descLabelStr: String
+    var textFieldFontSize: CGFloat = 17
+    var textFieldLineHeight: CGFloat = 25
+    var descLabelFontSize: CGFloat = 14
+    var assistantLabelFontSize: CGFloat = 12
+    var validCondition: (() -> Bool)?
+
+    init() {
+        self.descLabelStr = ""
+        self.inputContentType = .emailAddress
+        self.returnKeyType = .default
+        self.keyboardType = .default
+    }
+
+    init(descLabelStr: String,
+         inputContentType: UITextContentType = .emailAddress,
+         returnKeyType: UIReturnKeyType = .default,
+         keyboardType: UIKeyboardType = .default) {
+
+        self.descLabelStr = descLabelStr
+        self.inputContentType = inputContentType
+        self.returnKeyType = returnKeyType
+        self.keyboardType = keyboardType
+    }
+
+    var assistantLabelStr: String {
+        if inputIsValid {
+            return ""
+        }
+        return "안 괜찮아요"
+    }
+
+    var isSecureTextEntry: Bool {
+        return inputContentType == .password
+    }
+
+    var inputIsValid: Bool {
+        return validCondition?() ?? true
+    }
+}
