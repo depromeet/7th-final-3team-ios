69e8da7966b7a22dcfb92701bc77540cf07d6a02
Changnam Hong <hcn1519@gmail.com>
2020-01-19 20:01:47 +0900

    [#22] SignUp - ViewModel과 TextField View 구현
    - ViewModel에서 TextField ViewModel을 생성하도록 설정
    - 사용자 인풋 값 업데이트에 따른 viewModel 값 업데이트 처리
    - 값 변경에 따른 View 업데이트 처리

--- WatNi/UIWidgets/Signup/SignupViewModel.swift
+++ WatNi/UIWidgets/Signup/SignupViewModel.swift
@@ -11,38 +11,103 @@ import Combine
 
 class SignupViewModel: ObservableObject {
 
-    @Published var email: String = ""
-    @Published var password: String = ""
-    @Published var passwordConfirm: String = ""
+    enum InputType {
+        case name, email, password, passwordConfirm
+    }
+
+    private(set) var name: String = ""
+    private(set) var email: String = ""
+    private(set) var password: String = ""
+    private(set) var passwordConfirm: String = ""
 
     private var disposable = Set<AnyCancellable>()
 
-    init() {
-        $email.sink {
-            print("email", $0)
-        }.store(in: &disposable)
-
-        $password.sink {
-            print("password", $0)
-        }.store(in: &disposable)
-
-        $passwordConfirm.sink {
-            print("confirm", $0)
-        }.store(in: &disposable)
-    }
-
-    /// Validate Email
-    /// - Parameter candidate: Email String
+    /// 유효한 이메일 여부
     var isValidEmail: Bool {
         let expression = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let result = email.range(of: expression, options: .regularExpression)
 
-        return result != nil
+        return result != nil || email.isEmpty
     }
 
-    /// Validate Password
-    /// - Parameter candidate: Password String
+    /// 유효한 비밀번호 여부
     var isValidPassword: Bool {
-        return password.count >= 6 && password.count <= 12
+        return (password.count >= 6 && password.count <= 12) || password.isEmpty
+    }
+
+    /// 비밀번호 확인 입력이 비밀번호 입력과 동일 여부 확인
+    var passwordIsConfirmed: Bool {
+        return password == passwordConfirm || passwordConfirm.isEmpty
+    }
+
+    /// 각각의 TextField별 setter
+    /// - Parameters:
+    ///   - inputType: TextField 종류
+    ///   - newValue: 새로운 값
+    func update(_ inputType: InputType, newValue: String) {
+        switch inputType {
+        case .name:
+            name = newValue
+        case .email:
+            email = newValue
+        case .password:
+            password = newValue
+        case .passwordConfirm:
+            passwordConfirm = newValue
+        }
+    }
+
+    /// TextField에 대한 ViewModel 빌드
+    /// - Parameter inputType: TextField 종류
+    func createUnderlineViewModel(inputType: InputType) -> UnderlineTextFieldViewModel {
+        switch inputType {
+        case .name:
+            return UnderlineTextFieldViewModel(descLabelStr: "이름",
+                                               inputContentType: .name,
+                                               returnKeyType: .continue)
+        case .email:
+            let emailViewModel = UnderlineTextFieldViewModel(descLabelStr: "이메일",
+                                                             inputContentType: .emailAddress,
+                                                             returnKeyType: .continue,
+                                                             keyboardType: .asciiCapable)
+            emailViewModel.validCondition = { [weak self] input in
+                self?.isValidEmail ?? false
+            }
+            emailViewModel.assistantStr = invalidMessage(inputType: inputType)
+            return emailViewModel
+        case .password:
+            let passwordViewModel = UnderlineTextFieldViewModel(descLabelStr: "비밀번호",
+                                                                inputContentType: .password,
+                                                                returnKeyType: .continue)
+            passwordViewModel.validCondition = { [weak self] _ in
+                self?.isValidPassword ?? false
+            }
+            passwordViewModel.assistantStr = invalidMessage(inputType: inputType)
+            return passwordViewModel
+        case .passwordConfirm:
+            let passwordConfirmViewModel = UnderlineTextFieldViewModel(descLabelStr: "비밀번호 확인",
+                                                                       inputContentType: .password,
+                                                                       returnKeyType: .done)
+            passwordConfirmViewModel.validCondition = { [weak self] _ in
+                self?.passwordIsConfirmed ?? false
+            }
+            passwordConfirmViewModel.assistantStr = invalidMessage(inputType: inputType)
+            return passwordConfirmViewModel
+        }
+    }
+
+    /// 각각의 TextField별 에러 메시지
+    /// - Parameter inputType: TextField 종류
+    func invalidMessage(inputType: InputType) -> String {
+        switch inputType {
+        case .name:
+            return ""
+        case .email:
+            return "유효하지 않은 이메일 형식입니다."
+        case .password:
+            return "6-12자로 입력해주세요."
+        case .passwordConfirm:
+            return "비밀번호가 일치하지 않습니다."
+        }
     }
 }
