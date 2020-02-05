cca4d320d4fe7eb49c8f16f119004585d216b887
Changnam Hong <hcn1519@gmail.com>
2020-02-06 02:51:54 +0900

    [#60] 일정 등록 UI 작업

---
+++ WatNi/UIWidgets/CreatePlan/CreatePlanViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CreatePlanViewModel.swift
+//  WatNi
+//
+//  Created by 홍창남 on 2020/02/06.
+//  Copyright © 2020 hcn1519. All rights reserved.
+//
+
+import Foundation
+
+class CreatePlanViewModel {
+
+}
