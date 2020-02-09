15f834be5bd30791cdf2ae7594e9a6f97a8a80a4
Changnam Hong <hcn1519@gmail.com>
2020-02-09 18:11:55 +0900

    ImagePickerAccess를 일정 생성 화면에 반영

--- WatNi/UIWidgets/CreatePlan/CreatePlanViewModel.swift
+++ WatNi/UIWidgets/CreatePlan/CreatePlanViewModel.swift
@@ -7,7 +7,24 @@
 //
 
 import Foundation
+import Photos
 
 class CreatePlanViewModel {
 
+    func photoErrorMessage(error: PHPhotoLibrary.PhotoError) -> String {
+        switch error {
+        case .restricted:
+            return "사진 접근에 제약이 있습니다."
+        case .denied:
+            return "앱 설정에서 사진 접근을 허용해주세요."
+        }
+    }
+
+    func authStatus(completion: @escaping (Result<Void, PHPhotoLibrary.PhotoError>) -> Void) {
+        PHPhotoLibrary.authStatus { (result) in
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
+    }
 }
