1a0beba0ff0f82e0b801a49012bdb11f13a453bf
Changnam Hong <hcn1519@gmail.com>
2020-02-09 18:11:09 +0900

    ImagePickerAccess 추가 - UIImagePickerController 관리

--- WatNi/Module/ModelLayer/ImagePickerAccess.swift
+++ WatNi/Module/ModelLayer/ImagePickerAccess.swift
@@ -7,3 +7,61 @@
 //
 
 import Foundation
+import UIKit
+import Photos
+
+final class ImagePickerAccess: NSObject {
+
+    private let pickerController: UIImagePickerController
+    private weak var presentationController: UIViewController?
+
+    var didCancel: (() -> Void)?
+    var didSelectImage: ((_ image: UIImage?) -> Void)?
+
+    init(presentationController: UIViewController?, sourceType: UIImagePickerController.SourceType) {
+
+        let availableSourceType: UIImagePickerController.SourceType
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            availableSourceType = sourceType
+        } else {
+            availableSourceType = .photoLibrary
+        }
+
+        let pickerController = UIImagePickerController()
+        pickerController.sourceType = availableSourceType
+        pickerController.allowsEditing = true
+        pickerController.mediaTypes = ["public.image"]
+        self.pickerController = pickerController
+
+        super.init()
+        self.presentationController = presentationController
+        self.pickerController.delegate = self
+    }
+
+    func present() {
+        self.presentationController?.present(pickerController, animated: true, completion: nil)
+    }
+}
+
+extension ImagePickerAccess: UIImagePickerControllerDelegate {
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+        didCancel?()
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        guard let image = info[.editedImage] as? UIImage else {
+            picker.dismiss(animated: true, completion: nil)
+            didSelectImage?(nil)
+            return
+        }
+
+        picker.dismiss(animated: true, completion: nil)
+        didSelectImage?(image)
+    }
+}
+
+extension ImagePickerAccess: UINavigationControllerDelegate {
+
+}
