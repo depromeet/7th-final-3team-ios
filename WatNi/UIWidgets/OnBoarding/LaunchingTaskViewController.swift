81fc4c3ccff9f8d52eb28f880d6db6ecaae8d535
Changnam Hong <hcn1519@gmail.com>
2020-02-13 22:47:24 +0900

    최초 진입시 흐름 수정

--- WatNi/UIWidgets/OnBoarding/LaunchingTaskViewController.swift
+++ WatNi/UIWidgets/OnBoarding/LaunchingTaskViewController.swift
@@ -9,69 +9,62 @@
 import Foundation
 import UIKit
 
-class LaunchingTaskViewController: UIViewController {
+/// 초기 진입 화면 정의
+enum InitialScene {
+    /// 온보딩 화면
+    case onboarding
+    /// 모임 생성, 참여 이력이 없는 사용자 화면
+    case coach
+    /// 모임을 생성한 사용자
+    case home
+}
 
-    /// 초기 진입 화면 정의
-    enum InitialScene {
-        /// 온보딩 화면
-        case onboarding
-        /// 모임 생성, 참여 이력이 없는 사용자 화면
-        case coach
-        /// 모임을 생성한 사용자
-        case home
-    }
+class LaunchingTaskViewController: UIViewController {
 
     let worker = LaunchingTaskWorker()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        setupInitialViewController { [weak self] scene, memberMeta in
-//            let viewController: UIViewController
-//            switch scene {
-//            case .onboarding:
-//                viewController = OnBoardingViewController(nibName: OnBoardingViewController.className, bundle: nil)
-//            case .coach:
-//                let viewModel = CoachViewModel(memberMeta: memberMeta)
-//                viewController = CoachViewController(viewModel: viewModel, nibName: CoachViewController.className)
-//            case .home:
-//                let viewModel = HomeTabPagerViewModel(memberMeta: memberMeta)
-//                viewController = HomeTabPagerViewController(viewModel: viewModel,
-//                                                            nibName: HomeTabPagerViewController.className)
-//            }
-//            self?.navigationController?.pushViewController(viewController, animated: false)
-//        }
+        setupInitialViewController { [weak self] scene in
+            let viewController: UIViewController
+            switch scene {
+            case .onboarding:
+                viewController = OnBoardingPageViewController(transitionStyle: .scroll,
+                                                              navigationOrientation: .horizontal,
+                                                              options: nil)
+            case .coach:
+                let viewModel = CoachViewModel()
+                viewController = CoachViewController(viewModel: viewModel, nibName: CoachViewController.className)
+            case .home:
+                let viewModel = HomeTabPagerViewModel()
+                viewController = HomeTabPagerViewController(viewModel: viewModel,
+                                                            nibName: HomeTabPagerViewController.className)
+            }
+            self?.navigationController?.pushViewController(viewController, animated: false)
+        }
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-        let viewModel = HomeTabPagerViewModel(memberMeta: [])
-        let viewController = HomeTabPagerViewController(viewModel: viewModel,
-                                                        nibName: HomeTabPagerViewController.className)
-        navigationController?.pushViewController(viewController, animated: false)
-    }
-
-
-    private func setupInitialViewController(completionHandler: @escaping (InitialScene, [MemberMeta]) -> Void) {
+    private func setupInitialViewController(completionHandler: @escaping (InitialScene) -> Void) {
 
         guard MemberAccess.default.isLogin else {
-            completionHandler(.onboarding, [])
+            completionHandler(.onboarding)
             return
         }
 
-        MemberAccess.default.refreshToken { [weak self] (result) in
+        worker.initialViewController { [weak self] (result) in
             switch result {
             case .failure:
-                completionHandler(.onboarding, [])
+                completionHandler(.onboarding)
             case .success:
-                self?.worker.userMetaData { result in
+                self?.worker.memberMetaData { result in
                     switch result {
                     case .failure(let error):
                         print(error.localizedDescription)
-                        completionHandler(.coach, [])
+                        completionHandler(.onboarding)
                     case .success(let metaData):
-                        completionHandler(.home, metaData)
+                        let scene: InitialScene = metaData.group.isEmpty ? .coach : .home
+                        completionHandler(scene)
                     }
                 }
             }
