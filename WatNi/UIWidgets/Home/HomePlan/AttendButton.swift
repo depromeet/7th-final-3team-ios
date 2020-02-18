d70c2cd6d9fd33db9f4ce73a81b1291e0e7dcacb
Changnam Hong <hcn1519@gmail.com>
2020-02-19 00:06:35 +0900

    출석 버튼 디자인 반영

---
+++ WatNi/UIWidgets/Home/HomePlan/AttendButton.swift
@@ -0,0 +1,80 @@
+//
+//  AttendButton.swift
+//  WatNi
+//
+//  Created by 홍창남 on 2020/02/18.
+//  Copyright © 2020 hcn1519. All rights reserved.
+//
+
+import UIKit
+
+class AttendButton: UIButton {
+
+    enum AttendState {
+        case before
+        case available
+        case finish
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        layer.cornerRadius = 24
+        adjustsImageWhenDisabled = false
+    }
+
+    func setupTitle(state: AttendState, beforeDays: Int?) {
+        let attributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor.white,
+                                                         .font: UIFont.spoqaFont(ofSize: 15, weight: .bold),
+                                                         .kern: 1.25]
+
+        let title = buttonTitle(state: state, beforeDays: beforeDays)
+        let attributedString = NSAttributedString(string: title, attributes: attributes)
+        let image = buttomImage(state: state)
+
+        setImage(image, for: .normal)
+        setAttributedTitle(attributedString, for: .normal)
+        backgroundColor = buttonBackgroundColor(state: state)
+        isEnabled = state == .available
+    }
+
+    private func buttonTitle(state: AttendState, beforeDays: Int?) -> String {
+        switch state {
+        case .before:
+            let days = beforeDays ?? 0
+            return "출석 D-\(days)"
+        case .available:
+            return "왔어요!"
+        case .finish:
+            return "출석 완료"
+        }
+    }
+
+    private func buttomImage(state: AttendState) -> UIImage? {
+        switch state {
+        case .before:
+            return nil
+        case .available:
+            return #imageLiteral(resourceName: "ic_camera")
+        case .finish:
+            return #imageLiteral(resourceName: "attendFin")
+        }
+    }
+
+    private func buttonBackgroundColor(state: AttendState) -> UIColor {
+        switch state {
+        case .before, .finish:
+            return UIColor(decimalRed: 34, green: 34, blue: 34, alpha: 0.3)
+        case .available:
+            return WNColor.primaryRed
+        }
+    }
+}
