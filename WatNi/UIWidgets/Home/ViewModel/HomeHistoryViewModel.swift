63b1d4c646cd10afa59a3e9b8e282592ebb243eb
Changnam Hong <hcn1519@gmail.com>
2020-02-20 03:08:02 +0900

    출석기록 - ViewModel 설정

--- WatNi/UIWidgets/Home/ViewModel/HomeHistoryViewModel.swift
+++ WatNi/UIWidgets/Home/ViewModel/HomeHistoryViewModel.swift
@@ -7,7 +7,58 @@
 //
 
 import Foundation
+import SwiftyJSON
+import Combine
 
-class HomeHistoryViewModel: HomeTabViewModel {
+enum HomeHistorySectionType {
+    case filter, attend
+}
+
+class HomeHistoryViewModel: HomeTabViewModel, CollectionViewModelBase {
+
+    typealias BaseModel = WNAttendance
+    typealias SectionType = HomeHistorySectionType
+    typealias ModelCollection = [WNAttendance]
+    typealias CellModel = Array<CollectionViewCellModel>.Element
+    typealias ReusableViewModel = CollectionViewReusableViewModel
+
     let tabTitle = "출석 기록"
+
+    var userGroups: [WNGroup]
+    var models: [WNAttendance] = [] {
+        didSet {
+
+            cellModels = models.map { attendance in
+                return HomeHistoryCollectionViewCellModel(attendance: attendance)
+            }
+            cellModels.insert(HomeHistoryFilterCollectionViewCellModel(), at: 0)
+            reusableViewModels = [HomePlanCollectionHeaderViewModel()]
+        }
+    }
+    var cellModels: [CollectionViewCellModel] = []
+    var reusableViewModels: [CollectionViewReusableViewModel] = []
+
+    private var cancelables = Set<AnyCancellable>()
+
+    init(groups: [WNGroup]) {
+        self.userGroups = groups
+    }
+}
+
+extension HomeHistoryViewModel: HasCellModel {
+    func cellModel(indexPath: IndexPath) -> CollectionViewCellModel? {
+        return cellModels[safe: indexPath.row]
+    }
+}
+
+extension HomeHistoryViewModel: HasReusableViewModel {
+    func reusableViewModel(sectionType: HomeHistorySectionType) -> CollectionViewReusableViewModel? {
+        return reusableViewModels.first
+    }
+}
+
+extension HomeHistoryViewModel {
+    var numberOfItems: Int {
+        return cellModels.count
+    }
 }
