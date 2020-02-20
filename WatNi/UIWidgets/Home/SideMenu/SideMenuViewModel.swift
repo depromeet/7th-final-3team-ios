9e340f357b58ccdfe1c5e5ffbfcdbfa179395f53
Changnam Hong <hcn1519@gmail.com>
2020-02-21 02:44:44 +0900

    홈 내정보 - SideMenu 추가

---
+++ WatNi/UIWidgets/Home/SideMenu/SideMenuViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SideMenuViewModel.swift
+//  WatNi
+//
+//  Created by 홍창남 on 2020/02/21.
+//  Copyright © 2020 hcn1519. All rights reserved.
+//
+
+import Foundation
+
+class SideMenuViewModel {
+
+}
