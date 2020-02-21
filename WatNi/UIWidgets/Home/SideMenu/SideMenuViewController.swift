eb5ffe29ba0a4a07166988ae1d5ec67566f75a00
Changnam Hong <hcn1519@gmail.com>
2020-02-21 21:58:41 +0900

    내 정보 - 데모용 UI 구성

--- WatNi/UIWidgets/Home/SideMenu/SideMenuViewController.swift
+++ WatNi/UIWidgets/Home/SideMenu/SideMenuViewController.swift
@@ -13,7 +13,17 @@ class SideMenuViewController: UIViewController, ViewModelInjectable {
 
     typealias ViewModel = SideMenuViewModel
 
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var emailLabel: UILabel!
+    @IBOutlet private weak var managerLabel: UILabel!
+    @IBOutlet private weak var createPlanButton: UIButton!
+
+    @IBOutlet weak var registerHeightConstraint: NSLayoutConstraint!
+
     let viewModel: SideMenuViewModel
+
+    var didTapCreatePlanButton: (() -> Void)?
+    var didTapLogoutButton: (() -> Void)?
 
     required init(viewModel: ViewModel, nibName: String) {
         self.viewModel = viewModel
@@ -28,5 +38,27 @@ class SideMenuViewController: UIViewController, ViewModelInjectable {
         super.viewDidLoad()
 
         self.navigationController?.navigationBar.isHidden = true
+
+        managerLabel.text = viewModel.managerStr
+        nameLabel.text = viewModel.nameStr
+        emailLabel.text = viewModel.emailStr
+
+        // TODO: 데모용, 추후 제거
+        if !viewModel.isManager {
+            createPlanButton.isHidden = true
+            registerHeightConstraint.constant = 0
+        }
+    }
+
+    @IBAction func createPlanBtnTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: { [weak self] in
+            self?.didTapCreatePlanButton?()
+        })
+    }
+
+    @IBAction func logoutBtnTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: { [weak self] in
+            self?.didTapLogoutButton?()
+        })
     }
 }
--- WatNi/UIWidgets/Home/SideMenu/SideMenuViewModel.swift
+++ WatNi/UIWidgets/Home/SideMenu/SideMenuViewModel.swift
@@ -10,4 +10,19 @@ import Foundation
 
 class SideMenuViewModel {
 
+    var isManager: Bool {
+        return  MemberAccess.default.memberMeta?.isManager ?? false
+    }
+
+    var managerStr: String {
+        return isManager ? "관리자" : "회원"
+    }
+
+    var nameStr: String {
+        return MemberAccess.default.memberMeta?.member.name ?? ""
+    }
+
+    var emailStr: String {
+        return MemberAccess.default.memberMeta?.member.email ?? ""
+    }
 }
